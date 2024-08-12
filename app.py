17a1365a948f5fa63814d776d10aa4a4a92dd8f1
unknown <learsi.isr@gmail.com>
2024-08-12 16:00:59 -0300

    ajuste no endpoint hello world

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/hello-world')
 def hello():
-    return '<h1>Hello World!</h1>' 
+    return '<h1>Hello World!</h1>'
 
 @app.route('/<name>')
 def name(name):
